3c718ec5b34804d431a9597df7075a77e715a616
Adam-Moujar <amoujarbakhti@gmail.com>
2022-09-28 15:12:16 +0100

    Add exception handling to prime list generator

--- primes.py
+++ primes.py
@@ -3,4 +3,18 @@
 
 def primes(number_of_primes):
     list = []
+    count = 2
+    if number_of_primes <= 0:
+        raise ValueError("Only positive numbers are allowed!")
+    while(len(list) < number_of_primes):
+        if checkIfPrime(count):
+            list.append(count)
+        count = count+1
     return list
+
+def checkIfPrime(num):
+    isPrime = True
+    for i in range(2,num):
+        if num != i and num % i == 0:
+            isPrime = False
+    return isPrime
